ae8ab30394daf7076dd32f750dc01d833706f76c
Sookeyy-12 <thatonesuket@gmail.com>
2026-03-17 02:58:07 +0530

    update agent and system prompt.

---
+++ agent/agent.py
@@ -0,0 +1,23 @@
+class Agent:
+    def __init__(self, client, system):
+        self.client = client
+        self.system = system
+        self.messages = [] # list of messages 
+        
+        if self.system is not None:
+            self.messages.append({"role": "system", "content": self.system})
+
+    def __call__(self, message):
+        if message:
+            self.messages.append({"role": "user", "content": message})
+        
+        result = self.execute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+
+    def execute(self):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=self.messages,
+        )
+        return response.text
